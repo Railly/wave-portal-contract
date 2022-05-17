2dfe1a308f955796b574c9688475f7a780484680
RaillyH <97618491+RaillyH@users.noreply.github.com>
2022-05-17 00:07:02 -0500

    feat: customize smart contract & deploy

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,32 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+  uint256 totalWaves;
+  mapping(address => uint256) balances;
+
   constructor() {
     console.log("Heyy, I am a fancy smart contract :D!");
+  }
+
+  function wave(address _addr) public {
+    balances[_addr]++;
+    totalWaves++;
+    console.log("/******************* wave() *********************/");
+    console.log("%s has waved", msg.sender);
+    console.log("\n");
+  }
+
+  function getTotalWaves () public view returns(uint256) {
+    console.log("/*************** getTotalWaves() ********************/");
+    console.log("We have %d total waves!", totalWaves);
+    console.log("\n");
+    return totalWaves;
+  }
+
+  function getTotalWavesByAddress(address _addr) public view returns(uint256) {
+    console.log("/*************** getTotalWavesByAddress() ********************/");
+    console.log("%s has waved %d times!", _addr, balances[_addr]);
+    console.log("\n");
+    return balances[_addr];
   }
 } 
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,34 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
-  console.log(`Contract deployed to: ${waveContract.address}`);
+
+  console.log("Contract deployed to: ", waveContract.address);
+  console.log("Contract deployed by: ", owner.address);
+
+  let waveCount;
+
+  let waveTxn = await waveContract.wave(owner.address);
+  await waveTxn.wait();
+
+  waveTxn = await waveContract.wave(owner.address);
+  await waveTxn.wait();
+
+  await waveContract.getTotalWavesByAddress(owner.address);
+
+  waveTxn = await waveContract.connect(randomPerson).wave(randomPerson.address);
+  await waveTxn.wait();
+
+  waveTxn = await waveContract.connect(randomPerson).wave(randomPerson.address);
+  await waveTxn.wait();
+
+  waveTxn = await waveContract.connect(randomPerson).wave(randomPerson.address);
+  await waveTxn.wait();
+
+  await waveContract.getTotalWavesByAddress(randomPerson.address);
+
+  waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
